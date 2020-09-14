521527aa0131d2a7c6ff28b1c9906079e01e0116
John Kouris <jdkouris@gmail.com>
2020-09-14 15:49:59 -0700

    Presenting action sheet to select a photo from camera or photo library

--- photoapp/MainTabBarController.swift
+++ photoapp/MainTabBarController.swift
@@ -15,5 +15,75 @@ class MainTabBarController: UITabBarController {
 
         // Do any additional setup after loading the view.
     }
+    
+    override func tabBar(_ tabBar: UITabBar, didSelect item: UITabBarItem) {
+        // Detect which tab was tapped
+        if item.tag == 2 { // Camera tab
+            // Create the action sheet
+            let actionSheet = UIAlertController(title: "Add a Photo", message: "Select a source:", preferredStyle: .actionSheet)
+            
+            // Only add the camera button if it's available
+            if UIImagePickerController.isSourceTypeAvailable(.camera) {
+                // Create and add the Camera button
+                let cameraButton = UIAlertAction(title: "Camera", style: .default) { (action) in
+                    // Display the UIImagePickercontroller set to camera mode
+                    self.showImagePickerController(mode: .camera)
+                }
+                actionSheet.addAction(cameraButton)
+            }
+            
+            // Only add the library button if it's available
+            if UIImagePickerController.isSourceTypeAvailable(.photoLibrary) {
+                // Create and add the Photo Library button
+                let libraryButton = UIAlertAction(title: "Photo Library", style: .default) { (action) in
+                    // display the UIImagePickerController set to library mode
+                    self.showImagePickerController(mode: .photoLibrary)
+                }
+                actionSheet.addAction(libraryButton)
+            }
+            
+            // Create and add the cancel button
+            let cancelButton = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+            actionSheet.addAction(cancelButton)
+            
+            // Display the action sheet
+            present(actionSheet, animated: true, completion: nil)
+        }
+    }
+    
+    func showImagePickerController(mode: UIImagePickerController.SourceType) {
+        // Create the picker and set the mode
+        let imagePicker = UIImagePickerController()
+        imagePicker.sourceType = mode
+        
+        // Set the tab bar controller as the delegate
+        imagePicker.delegate = self
+        
+        // Present the image picker
+        present(imagePicker, animated: true, completion: nil)
+    }
 
+}
+
+extension MainTabBarController: UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        // Dismiss the image picker
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        // Get a reference to the selected photo
+        let selectedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage
+        
+        // Check that the selected image isn't nil
+        if let selectedImage = selectedImage {
+            // Upload it
+            
+        }
+        
+        // Dismiss the image picker
+        dismiss(animated: true, completion: nil)
+    }
+    
 }
