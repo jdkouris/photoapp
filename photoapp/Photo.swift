8d852cf1c02a24cc9e85eacd8c5136fd2c77644a
John Kouris <jdkouris@gmail.com>
2020-09-14 16:01:29 -0700

    Added photo model and photo service

---
+++ photoapp/Photo.swift
@@ -0,0 +1,17 @@
+//
+//  Photo.swift
+//  photoapp
+//
+//  Created by John Kouris on 9/14/20.
+//  Copyright © 2020 John Kouris. All rights reserved.
+//
+
+import Foundation
+
+struct Photo {
+    var photoId: String?
+    var byId: String?
+    var byUsername: String?
+    var date: String?
+    var url: String?
+}
---
+++ photoapp/Services/PhotoService.swift
@@ -0,0 +1,30 @@
+//
+//  PhotoService.swift
+//  photoapp
+//
+//  Created by John Kouris on 9/14/20.
+//  Copyright © 2020 John Kouris. All rights reserved.
+//
+
+import UIKit
+import FirebaseStorage
+import FirebaseFirestore
+
+class PhotoService {
+    
+    static func savePhoto(image: UIImage) {
+        // Get the data representation of the UIImage
+        
+        
+        // Create a filename
+        
+        // Get the user id of the current user
+        
+        // Create a firebase storage path/reference
+        
+        // Upload the data
+        
+        // Upon successful upload, create the database entry
+    }
+    
+}
