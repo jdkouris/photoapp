acb9e61d9771ae697ab529016c1f7bb6870587e2
John Kouris <jdkouris@gmail.com>
2020-09-14 11:32:24 -0700

    Added UserService

---
+++ photoapp/Services/UserService.swift
@@ -0,0 +1,17 @@
+//
+//  UserService.swift
+//  photoapp
+//
+//  Created by John Kouris on 9/14/20.
+//  Copyright © 2020 John Kouris. All rights reserved.
+//
+
+import Foundation
+
+class UserService {
+    
+    static func retrieveProfile(userID: String) {
+        
+    }
+    
+}
