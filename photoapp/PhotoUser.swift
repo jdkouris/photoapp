82805d461ffe3d52af0b9a3868b3c8d75f53843f
John Kouris <jdkouris@gmail.com>
2020-09-14 11:31:21 -0700

    Added PhotoUser

---
+++ photoapp/PhotoUser.swift
@@ -0,0 +1,14 @@
+//
+//  PhotoUser.swift
+//  photoapp
+//
+//  Created by John Kouris on 9/14/20.
+//  Copyright © 2020 John Kouris. All rights reserved.
+//
+
+import Foundation
+
+struct PhotoUser {
+    var userId: String?
+    var username: String?
+}
