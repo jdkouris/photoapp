12094126a64aba478eea0553cb0b56900c43018f
John Kouris <jdkouris@gmail.com>
2020-09-14 15:22:43 -0700

    Added MainTabBarController file

---
+++ photoapp/MainTabBarController.swift
@@ -0,0 +1,19 @@
+//
+//  MainTabBarController.swift
+//  photoapp
+//
+//  Created by John Kouris on 9/14/20.
+//  Copyright © 2020 John Kouris. All rights reserved.
+//
+
+import UIKit
+
+class MainTabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
